cc46f7099e194282764857037fedb6378e601767
Ted Ly <ted.ly@ansarada.com>
2024-06-27 01:38:20 +0700

    Add federation support.

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,57 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSubgraphSchema } from '@apollo/subgraph';
+import gql from 'graphql-tag';
 
-const typeDefs = `#graphql
-  type Me {
-    email: String
-    id: String
-    firstName: String
-    lastName: String
-  }
+async function startApolloServer() {
+  const app = express();
 
-  type Query {
-    me: Me
-  }
-`;
+  const typeDefs = gql`
+    extend schema
+      @link(
+        url: "https://specs.apollo.dev/federation/v2.0"
+        import: ["@key", "@shareable"]
+      )
 
-const me = {
-  email: "johndoe@example.com",
-  id: "123e4567-e89b-12d3-a456-426614174000",
-  firstName: "John",
-  lastName: "Doe",
-};
+    type Profile @key(fields: "id") {
+      id: String
+      email: String
+      firstName: String
+      lastName: String
+    }
 
-const resolvers = {
-  Query: {
-    me: () => me,
-  },
-};
+    type Query {
+      me: Profile
+    }
+  `;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+  const me = {
+    id: "123e4567-e89b-12d3-a456-426614174000",
+    email: "johndoe@example.com",
+    firstName: "John",
+    lastName: "Doe",
+  };
 
-await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  const resolvers = {
+    Query: {
+      me: () => me,
+    },
+  };
+
+  const server = new ApolloServer({
+    schema: buildSubgraphSchema({ typeDefs, resolvers }),
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  const port = process.env.PORT || 4000;
+  app.listen({ port }, () =>
+    console.log(`🚀  Server ready at http://localhost:${port}/graphql`)
+  );
+}
+
+startApolloServer().catch(err => {
+  console.error('Error starting server:', err);
 });
